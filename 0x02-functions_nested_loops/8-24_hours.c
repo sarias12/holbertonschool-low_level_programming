1ae3769aa9bdeacfdb077606cd9edc262b4f087c
sarias12 <1638@holbertonschool.com>
2020-02-12 22:12:12 +0000

    8-task

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+  * jack_bauer - Entry point
+  * Write a function that prints every minute of the day of Jack Bauer,
+  *starting from 00:00 to 23:59
+  *
+  *
+  * Return: Always 0 (Success)
+  */
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0 ; h <= 23 ; h++)
+	{
+		for (m = 0 ; m <= 59 ; m++)
+		{
+			_putchar(h / 10 + '0');
+			_putchar(h % 10 + '0');
+			_putchar(58);
+			_putchar(m / 10 + '0');
+			_putchar(m % 10 + '0');
+			_putchar('\n');
+		}
+		
+	}
+}
