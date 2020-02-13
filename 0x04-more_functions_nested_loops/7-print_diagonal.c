bb2a32008fa669ac741411a8f60b401a2b38b9e9
sarias12 <1638@holbertonschool.com>
2020-02-13 22:50:51 +0000

    7-task

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * print_diagonal - Entry point
+ * @n: - description
+ *
+ * Return: Always 0 (Success)
+ */
+void print_diagonal(int n)
+{
+	int tab, line;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (tab = 0 ; tab < n ; tab++)
+		{
+			for (line = 0 ; line < tab ; line++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);		
+			_putchar('\n');		
+		}
+	}
+}
