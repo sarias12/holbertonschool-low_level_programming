4cc0397b7201febd32545c22137d8ef99bb8c2d2
sarias12 <1638@holbertonschool.com>
2020-02-14 03:18:07 +0000

    9-task

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @size: - description
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 1 ; x <= 100 ; x++)
+	{
+		if (((x % 3) == 0) && ((x % 5) == 0))
+			printf("FizzBuzz ");
+		else if ((x % 3) == 0)
+			printf("Fizz ");
+		else if ((x % 5) == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", x);
+	}
+	printf("\n");
+	return(0);
+}
