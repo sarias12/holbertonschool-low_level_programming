6f77c8dde80d52a257309936403ff2e4af7afafc
sarias12 <1638@holbertonschool.com>
2020-02-20 03:46:37 +0000

    task-4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+  * print_rev - function that prints a string, followed by a new line.
+  *
+  * @s: variable
+  *
+  * Return: string in reverse
+  */
+void print_rev(char *s)
+{
+	int x;
+
+	for (x = 0 ; s[x] != '\0' ; x++)
+	{
+	}
+	while (x >= 0)
+	{
+		_putchar(s[x]);
+		x--;
+	}
+
+	_putchar('\n');
+}
