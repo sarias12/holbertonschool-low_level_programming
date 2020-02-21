a98b27402ea70b15f129f3194f882faa4b1fc51a
sarias12 <1638@holbertonschool.com>
2020-02-21 22:55:45 +0000

    task

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+  * *leet - function that encodes a string into 1337.
+  *
+  * @s: string
+  *
+  *
+  * Return: String Upper
+  */
+char *leet(char *s)
+{
+	int i;
+	char letter[]= { 'a', 'e', 'o', 't', 'l'};
+	char numbs[]
+
+
+
+
+	for (i = 0 ; s[i] < '\0' ; i++)
+		if ()
+
+	return (s);
+}
