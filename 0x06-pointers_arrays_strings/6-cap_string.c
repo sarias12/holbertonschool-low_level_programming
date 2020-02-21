6623e047a59581d0019423a83ddf89e11d87013c
sarias12 <1638@holbertonschool.com>
2020-02-21 02:59:22 +0000

    task-6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "holberton.h"
+/**
+  * *cap_string - function that capitalizes all words of a string..
+  *
+  * @tcap: string
+  *
+  *
+  * Return: String Upper
+  */
+char *cap_string(char *tcap)
+{
+	int x;
+	bool a, b, c;
+
+	for (x = 0 ; tcap[x] != '\0' ; x++)
+	{
+		a = (tcap[x] >= 97 && tcap[x] <= 122);
+		b = (tcap[x - 1] < 65 || tcap[x - 1] > 122);
+		c = (!(tcap[x - 1] > 47 && tcap[x - 1] < 58));
+		if (a && b && c)
+			tcap[x] -= 32;
+	}
+	return (tcap);
+}
