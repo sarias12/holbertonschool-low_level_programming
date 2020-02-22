26ef2a21e30038d28b41c7e9b963cae6c34f6fa2
sarias12 <1638@holbertonschool.com>
2020-02-22 04:42:14 +0000

    task-8

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+  * *rot13 - function that encodes a string using rot13.
+  *
+  * @s: string
+  *
+  *
+  * Return: String encode rot13
+  */
+char *rot13(char *s)
+{
+	int i, j;
+	char letter[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
+		'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+		'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+		'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+		'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+	char rot13[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+		'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e',
+		'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'N', 'O', 'P', 'Q',
+		'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+		'J', 'K', 'L', 'M'};
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		for (j = 0 ; j < 26 ; j++)
+		{
+			if (s[i] == letter[j])
+				s[i] = rot13[j];
+		}
+	}
+
+	return (s);
+}
