69034d40a41afe3db309c99fe5d8e7b6ae223712
sarias12 <1638@holbertonschool.com>
2020-02-27 21:00:18 +0000

    task-0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,11 +9,11 @@
   */
 void _puts_recursion(char *s)
 {
-	
-	_putchar(*s);
-	s++;
 	if (*s != '\0')
 	{
-		_puts_recursion(s);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
+	else
+		_putchar('\n');
 }
