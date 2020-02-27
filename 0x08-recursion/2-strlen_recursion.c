ae7cdf09f3354523965b5d963667735cda1f40eb
sarias12 <1638@holbertonschool.com>
2020-02-27 22:11:24 +0000

    task-2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+  * _strlen_recursion - function that fills memory with a constant byte.
+  *
+  * @s: string
+  *
+  * Return: lenth
+  */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return(0);
+}
