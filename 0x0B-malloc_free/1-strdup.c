6e7a27a8fea81d9c611f63b58d86de730faffe63
sarias12 <1638@holbertonschool.com>
2020-03-03 19:40:00 +0000

    task-1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,13 +15,15 @@ char *_strdup(char *str)
 	int i, j;
 	char *vec;
 
-	if (str == NULL)
+	if (str == 0)
 		return (NULL);
 	for (i = 0 ; str[i] != '\0' ; i++)
 	{
 	}
 	i++;
 	vec = malloc(sizeof(char) * i);
+	if (vec == NULL)
+		return (NULL);
 	for (j = 0 ; j < i ; j++)
 	{
 		vec[j] = str[j];
