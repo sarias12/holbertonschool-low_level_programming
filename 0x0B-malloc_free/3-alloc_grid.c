127e1414e484b6fdb04e0c12a1d8a6cb12f7f98a
sarias12 <1638@holbertonschool.com>
2020-03-04 21:02:19 +0000

    task-2

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,6 +17,15 @@ char *str_concat(char *s1, char *s2)
 	int i, j, k, m;
 	char *vec;
 
+	for (i = 0 ; s1[i] != '\0' ; i++)
+	{
+	}
+	for (j = 0 ; s2[j] != '\0' ; j++)
+	{
+	}
+	vec = malloc(sizeof(char) * (i + j + 1));
+	if (vec == NULL)
+		return (NULL);
 	if (s1 == NULL)
 	{
 		s1 = "";
@@ -25,15 +34,6 @@ char *str_concat(char *s1, char *s2)
 	{
 		s2 = "";
 	}
-	for (i = 0 ; s1[i] != '\0' ; i++)
-	{
-	}
-	for (j = 0 ; s2[j] != '\0' ; j++)
-	{
-	}
-	vec = malloc(sizeof(char) * (i + j) + 1);
-	if (vec == NULL)
-		return (NULL);
 	for (k = 0 ; k < i ; k++)
 		vec[k] = s1[k];
 	m = 0;
