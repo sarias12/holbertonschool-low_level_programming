db5586441f7acad48137b0144a01ceac633f3a00
sarias12 <1638@holbertonschool.com>
2020-03-05 20:11:44 +0000

    task-1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * string_nconcat - Entry point
+ *
+ * @s1: - description
+ * @s2: - description
+ * @n: - description
+ *
+ * Return: Always 0 (Success)
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j, k, m, size;
+	char *vec;
+
+	if (s1 == NULL || s2 == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0 ; s1[i] != '\0' ; i++)
+	{
+	}
+	for (m = 0 ; s2[m] != '\0' ; m++)
+	{
+	}
+	if (n >= m)
+	{
+		size = m + i;
+	}
+	else
+	{
+		size = i + n;
+	}
+	vec = malloc(sizeof(char) * size + 1);
+	if (vec == NULL)
+	{
+		free(vec);
+		return (NULL);
+	}
+	for (j = 0 ; j < i ; j++)
+	{
+		vec[j] = s1[j];
+	}
+	for (k = 0 ; j < size ; j++)
+	{
+		vec[j] = s2[k];
+		k++;
+	}
+	vec[j] = '\0';
+	return (vec);
+}
