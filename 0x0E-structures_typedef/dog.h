593f9163d4821b0e103b3e5b0ea551dd785f95c6
sarias12 <1638@holbertonschool.com>
2020-03-09 21:59:59 +0000

    task-0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
-#ifndef can
-#define can
-struct dog 
+#ifndef CAN
+#define CAN
+/**
+* struct dog - attributes
+* @name: First attribute
+* @age: Second attribute
+* @owner: Third attribute
+* Description: Attribute's dog.
+*/
+struct dog
 {
 	char *name;
 	float age;
