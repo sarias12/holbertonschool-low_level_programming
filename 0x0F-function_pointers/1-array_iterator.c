499307647592026abe678436a44b660d7f2d464b
sarias12 <1638@holbertonschool.com>
2020-03-11 19:25:44 +0000

    task-1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - Entry point
+ *
+ * @array: - description
+ * @size: - description
+ * @action: - description
+ *
+ *Return: Always 0 (Success)
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+
+{
+	unsigned int i;
+
+	if (action == NULL)
+		return;
+	for (i = 0 ; i < size ; i++)
+		action(array[i]);
+}
+
