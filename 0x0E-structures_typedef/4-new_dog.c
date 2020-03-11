95170d5d95f595eca3c90394b7767f8558d9142f
sarias12 <1638@holbertonschool.com>
2020-03-11 00:05:28 +0000

    task-4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,7 +15,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *secondog;
 	int i, j, l, k;
-	char *name2, *owner2;
 
 	secondog = malloc(sizeof(struct dog));
 	if (secondog == NULL)
@@ -24,30 +23,28 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	for (i = 0 ; name[i] != '\0' ; i++)
 		;
-	name2 = malloc(sizeof(char) * i + 1);
-		if (name2 == NULL)
+	secondog->name = malloc(sizeof(char) * i + 1);
+		if (secondog->name == NULL)
 		{
 			free(secondog);
 			return (NULL);
 		}
 	for (k = 0 ; name[k] != '\0' ; k++)
-		name2[k] = name[k];
-	name2[k] = '\0';
-	secondog->name = name2;
-	secondog->age = age;
+		secondog->name[k] = name[k];
+	secondog->name[k] = '\0';
 	for (j = 0 ; owner[j] != '\0' ; j++)
 		;
-	owner2 = malloc(sizeof(char) * i + 1);
-		if (owner2 == NULL)
+	secondog->owner = malloc(sizeof(char) * i + 1);
+		if (secondog->owner == NULL)
 		{
-			free(name2);
+			free(secondog->name);
 			free(secondog);
 			return (NULL);
 		}
 	for (l = 0 ; owner[l] != '\0' ; l++)
-		owner2[l] = owner[l];
-	owner2[l] = '\0';
-	secondog->owner = owner2;
+		secondog->owner[l] = owner[l];
+	secondog->owner[l] = '\0';
+	secondog->age = age;
 	return (secondog);
 }
 
