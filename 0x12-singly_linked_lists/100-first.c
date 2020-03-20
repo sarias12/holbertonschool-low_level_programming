1379b1bf5d8aca4a3881a26ed4554879ffbc6792
sarias12 <1638@holbertonschool.com>
2020-03-20 03:15:07 +0000

    task-4 an two advances

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * Myfunction -a function that prints a string
+ * before the main function is executed.
+ *
+ * Return: nothing.
+ */
+void Myfunction(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,15 +1,15 @@
 #include "lists.h"
 /**
  * free_list - function that frees a list_t list.
- *
+ * @head: list
  * Return: nothing.
  */
 void free_list(list_t *head)
 {
-    while (head != NULL)
-    {
-        free(head->str);
-        free(head);
-        head = head->next;
-    }
+	while (head != NULL)
+	{
+		free(head->str);
+		free(head);
+		head = head->next;
+	}
 }
