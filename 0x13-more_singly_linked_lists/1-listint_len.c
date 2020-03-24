1f41744fbb30ca61ea2deb08a672b40fb104fb25
sarias12 <1638@holbertonschool.com>
2020-03-24 21:29:52 +0000

    task-1 and  update header file

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -4,13 +4,13 @@
  * @h: list.
  * Return: the number of nodes.
  */
-size_t listint_len(const listint_t *h);
+size_t listint_len(const listint_t *h)
 {
 	int i = 0;
 
 	while (h != NULL)
 	{
-        i++;
+		i++;
 		h = h->next;
 	}
 	return (i);
