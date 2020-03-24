4756436146a4b7910056ad4c038362d641fe0e2c
sarias12 <1638@holbertonschool.com>
2020-03-24 22:02:09 +0000

    task-2 and  update header file

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,23 +1,22 @@
 #include "lists.h"
 /**
  * add_nodeint - function that adds a new node at the beginning
- * of a list_t list.
+ * of a listint_t list.
  * @head: address list.
  * @n: integer.
  * Return: the number of nodes.
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	list_t *new_element;
+	listint_t *new_element;
 
-	new_element = malloc(sizeof(list_t));
+	new_element = malloc(sizeof(listint_t));
 	if (new_element == NULL)
 	{
 		return (NULL);
 	}
 	else
 	{
-
 		new_element->n = n;
 		new_element->next = *head;
 		*head = new_element;
