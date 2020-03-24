81ab6ebd36f228fcb43339bff6e0f630e34c14d3
sarias12 <1638@holbertonschool.com>
2020-03-24 21:26:09 +0000

    task-1 and  update header file

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * listint_len - function that prints all the elements of a list_t list.
+ * @h: list.
+ * Return: the number of nodes.
+ */
+size_t listint_len(const listint_t *h);
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+        i++;
+		h = h->next;
+	}
+	return (i);
+}
