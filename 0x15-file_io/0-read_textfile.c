75590b4e42fdad641234be8dd91419abb7cec6d4
sarias12 <1638@holbertonschool.com>
2020-04-01 04:20:20 +0000

    0-task, header file and Readme

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+/**
+ * read_textfile - function that reads a text file and prints it to
+ * the POSIX standard output.
+ * @letters: the number of letters it should read and print.
+ * @filename: File Descriptor.
+ * Return: the actual number of letters it could read and print.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int flow, sz, dp;
+	char *text;
+
+	text = malloc(sizeof(char) * letters);
+	if (text == NULL)
+	{
+		return (0);
+	}
+	flow = open(filename, O_RDONLY);
+	if (flow < 0)
+	{
+		free(text);
+		return (0);
+	}
+	sz = read(flow, text, letters);
+	if (sz < 0)
+	{
+		close(flow);
+		free(text);
+		return (0);
+	}
+	dp = write(1, text, sz);
+	if (dp < 0 || sz != dp)
+	{
+		close(flow);
+		free(text);
+		return (0);
+	}
+	close(flow);
+	free(text);
+	return (sz);
+}
---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include "holberton.h"
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif
