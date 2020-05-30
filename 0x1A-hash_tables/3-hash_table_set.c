4058760db842bcfd00d02a442e693fef36ab9908
Sergio Steben Arias Quintero <1638@holbertonschool.com>
2020-05-30 00:47:41 +0000

    Task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node = NULL;
 	hash_node_t **ptr2_to_hash_node = NULL;
 
-	if (key == NULL || value == NULL || ht == NULL)
+	if (key == NULL || value == NULL || ht == NULL || ht->array == NULL)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	ptr2_to_hash_node = (ht->array);
