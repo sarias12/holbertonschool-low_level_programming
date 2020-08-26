f67da7c48b8b3ceff8f760a0bdb1a093e44d616d
sarias12 <1638@holbertonschool.com>
2020-08-26 23:14:32 +0000

    Big O Task 9  Interpolation search

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,36 @@
+#include "search_algos.h"
+/**
+* interpolation_search - Function interpolation_search.
+* @array: Pointer to the first element of the array
+* @size: Number of element in array.
+* @value: Value to search for.
+* Return: Position of value in array or -1 if fail.
+*/
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t low = 0;
+	size_t high = size - 1;
+	size_t mid;
+
+	if (array == NULL)
+		return (-1);
+
+	while ((array[high] != array[low]) &&
+		(value >= array[low]) && (value <= array[high]))
+	{
+		mid = low + (((double)(high - low) / (array[high] - array[low])) *
+			(value - array[low]));
+		printf("Value checked array[%d] = [%d]\n", (int) mid, (int) array[mid]);
+		if (array[mid] < value)
+			low = mid + 1;
+		else if (value < array[mid])
+			high = mid - 1;
+		else
+			return (mid);
+	}
+
+	if (value == array[low])
+		return (low);
+	printf("Value checked array[%d] = [%d]\n", (int) mid, (int) array[mid]);
+	return (-1);
+}
