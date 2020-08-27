bfbeb8b1269b2dc3973854b6f5088c8b3a52d626
sarias12 <1638@holbertonschool.com>
2020-08-27 00:19:04 +0000

    Big O Task 9  Interpolation search

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -33,6 +33,6 @@ int interpolation_search(int *array, size_t size, int value)
 		return (low);
 	mid = low + (((double)(high - low) / (array[high] - array[low])) *
 			(value - array[low]));
-		printf("Value checked array[%d] is out of range\n", (int) mid);
-		return (-1);
+	printf("Value checked array[%d] is out of range\n", (int) mid);
+	return (-1);
 }
